d7e297c78bb9e4e4c7ad63451e0451f397a5659e
akhilsreddy1 <47410806+akhilsreddy1@users.noreply.github.com>
2019-10-23 20:30:28 -0500

    Update vault-requests-kube.py

--- vault-requests-kube.py
+++ vault-requests-kube.py
@@ -6,6 +6,7 @@ import os
 import warnings
 import requests
 warnings.filterwarnings("ignore")
+import hvac
 
 vault_role = os.environ.get('VAULT_ROLE')
 vault_url = os.environ.get('VAULT_URL')
@@ -21,13 +22,16 @@ logging.basicConfig(
     datefmt='%d-%b-%y %H:%M:%S',
     level=logging.INFO)
 
+log = logging.getLogger(__name__) 
+
+
 if any(v in (None, '') for v in[vault_url, vault_role, secret_kv_path, secret_target_path, secret_target_file, secrets_type]):
-    logging.error("Environment Variables not passed incorrectly")
+    log.error("Environment Variables not passed incorrectly")
     raise SystemExit(1)
 
 if vault_skip_ssl in (None, False):
     certspath = False
-    logging.info("Skipping SSL while connecting to Vault")
+    log.info("Skipping SSL while connecting to Vault")
 else:
     certspath = '/etc/tls/ca.crt'
 
@@ -40,14 +44,13 @@ env_mode = True if secrets_type.lower() == 'variables' else False
 
 def get_kubernetes_token():
 
-    logging.info("Getting POD Default service account token")
+    log.info("Getting POD Default service account token")
 
     try:
         with open('/var/run/secrets/kubernetes.io/serviceaccount/token', mode='r') as f1:
             return f1.read()
     except IOError:
-        logging.exception(
-            'Default Service account token file not found in Pod')
+        log.exception('Default Service account token file not found in Pod')
         raise SystemExit(1)
 
 
@@ -56,7 +59,7 @@ def get_client_token():
     try:
         payload = {"jwt": get_kubernetes_token(), "role": vault_role}
 
-        logging.info("Getting Vault client token using k8s-auth-method ")
+        log.info("Getting Vault client token using k8s-auth-method ")
         response = requests.post(
             url=vault_url + '/v1/auth/'+vault_k8s_endpoint+'/login',
             data=json.dumps(payload),
@@ -64,27 +67,27 @@ def get_client_token():
             verify=certspath)
 
     except requests.ConnectionError as err:
-        logging.exception(
+        log.exception(
             f'Connectivity Error for Vault URL {vault_url} :: {err}')
         raise SystemExit(1)
     except Exception as err:
-        logging.exception(
+        log.exception(
             f'Could not get a Client Token while authenticating with Vault :: {err}')
         raise SystemExit(1)
 
     if response.status_code == requests.codes.ok:
         client_token = json.loads(response.content)['auth']['client_token']
-        logging.info('Client Token Fetched')
+        log.info('Client Token Fetched')
         return client_token
     else:
-        logging.error(
+        log.error(
             f'Invalid API request while authenticating with Vault. Status Code : {response.status_code} Response : {response.reason} {response.json()}')
         raise SystemExit(1)
 
 
 def get_secret_vault(client_token):
 
-    logging.info('Getting Secrets from Vault')
+    log.info('Getting Secrets from Vault')
 
     try:
         response = requests.get(
@@ -94,10 +97,10 @@ def get_secret_vault(client_token):
             verify=certspath)
 
     except requests.ConnectionError:
-        logging.exception(f'Connectivity Error for Vault URL {vault_url}')
+        log.exception(f'Connectivity Error for Vault URL {vault_url}')
         raise SystemExit(1)
     except Exception as err:
-        logging.exception(
+        log.exception(
             f'Could not retrieve secrets from Vault. Exception :: {err}')
         raise SystemExit(1)
 
@@ -106,9 +109,8 @@ def get_secret_vault(client_token):
         try:
             if file_mode:
                 with open((secret_target_path + os.sep + secret_target_file), 'w') as f1:
-                    f1.write(json.loads(response.content)
-                             ['data'][secret_target_file])
-                logging.info(
+                    f1.write(json.loads(response.content)['data'][secret_target_file])
+                log.info(
                     f'Secrets File written to : {secret_target_path}/{secret_target_file}')
 
             if env_mode:
@@ -116,26 +118,26 @@ def get_secret_vault(client_token):
                 with open((secret_target_path + os.sep + secret_target_file), 'w') as f1:
                     for k, v in secrets.items():
                         f1.write(k + '=' + "'" + v + "'" + '\n')
-                logging.info(
+                log.info(
                     f'Secret Variables written to : {secret_target_path}/{secret_target_file}')
         except IOError:
-            logging.exception(
+            log.exception(
                 'Secrets target path/file not found,or unable to open,Exiting')
             raise SystemExit(1)
         except Exception as err:
-            logging.exception(f'Error Writing Secrets :: {err}')
+            log.exception(f'Error Writing Secrets :: {err}')
             raise SystemExit(1)
     else:
-        logging.error(
+        log.error(
             f"Invalid API request while getting secrets from Vault. Status Code : {response.status_code} Response : {response.reason} {response.json()}")
         raise SystemExit(1)
 
 
 if __name__ == "__main__":
-    logging.info('Vault-Init Container Started')
+    log.info('Vault-Init Container Started')
 
     client_token = get_client_token()
 
     get_secret_vault(client_token)
 
-    logging.info('Vault-Init Container Completed')
+    log.info('Vault-Init Container Completed')
